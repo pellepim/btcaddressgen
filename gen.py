aff5dbd5f0dc2f500a77b59afe85ee4474eace1c
Jon Nylander <pellepim@gmail.com>
2017-01-03 23:13:56 +0100

    Generation of address sorta done

--- gen.py
+++ gen.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
+# -*- coding: ascii -*-
 
 import random
-import hashlib
+import bitcoin
 import base58
+from pybitcoin import BitcoinPrivateKey
 
 
 def private_key():
@@ -39,28 +40,47 @@ def private_wif(p_key_hex=None):
         p_key_hex = private_hex()
 
     private_key_and_version = '80' + p_key_hex
-    first_sha = hashlib.sha256(bytearray.fromhex(private_key_and_version)).hexdigest().upper()
-    second_sha = hashlib.sha256(bytearray.fromhex(first_sha)).hexdigest().upper()
-    checksum = second_sha[:8]
-    private_key_with_checksum = private_key_and_version + checksum
-    private_key_wif = base58.b58encode(str(bytearray.fromhex(private_key_with_checksum)))
+    private_key_wif = base58.b58encode_check(str(bytearray.fromhex(private_key_and_version)))
 
     return private_key_wif
 
 
 def wif_to_pk(wif):
-    print wif
+    """
+    Returns a 32 byte long private key based on a supplied Wallet Input Format string.
+    """
     byte_string = base58.b58decode_check(wif)
-    print byte_string
-    checksum = byte_string[-8:]
+    return [ord(b) for b in byte_string][1:]
 
-    first_sha = hashlib.sha256(byte_string[0:-8]).hexdigest()
-    second_sha = hashlib.sha256(first_sha).hexdigest()
 
-    if checksum == second_sha[:8] and byte_string[0:1] == '80':
-        return byte_string[2:-8]
+def wif_to_pk_hex(wif):
+    """
+    Returns a hex private key based on a supplied Wallet Input Format string
+    """
+    return private_hex(wif_to_pk(wif))
 
-    raise ValueError('Invalid WIF')
+
+def public_key(pk=None, wif=None, _hex=None):
+    if (pk is None and wif is None and _hex is None) or (pk and wif and _hex):
+        raise RuntimeError('Supply either private key either as bytearray (pk) or in Wallet Input Format (wif), not both.')
+
+    if wif:
+        pk = wif_to_pk(wif)
+
+    if pk:
+        _hex = private_hex(pk)
+
+    return BitcoinPrivateKey(_hex).public_key().address()
+
+print BitcoinPrivateKey('0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D').public_key().address()
+
+
+
+
+
+
+
+
 
 
 
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: ascii -*-
 
 import unittest
 import gen
@@ -29,7 +29,6 @@ class PrivateHexKeyTests(unittest.TestCase):
 
     def test_new(self):
         """ Test that we can generate a hex representation private key randomly """
-
         actual = gen.private_hex()
 
         self.assertEqual(len(bytearray.fromhex(actual)), 32)  # The private key ought to be 32 bytes long
@@ -60,10 +59,35 @@ class PrivateWIFKeyTests(unittest.TestCase):
 class WIFToPKTests(unittest.TestCase):
     def setUp(self):
         self.mocked_wif = '5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ'
-        self.expected_hex = '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
+        self.expected_pk = [12, 40, 252, 163, 134, 199, 162, 39, 96, 11, 47, 229, 11, 124, 174, 17, 236, 134, 211, 191,
+                            31, 190, 71, 27, 232, 152, 39, 225, 157, 114, 170, 29]
+        self.expected_pk_hex = '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
 
-    def test(self):
-        self.assertEquals(gen.wif_to_pk(self.mocked_wif), self.expected)
+    def test_to_pk(self):
+        """ Test that decoding a WIF string to a private key bytearray works as intended"""
+        self.assertEquals(gen.wif_to_pk(self.mocked_wif), self.expected_pk)
+
+    def test_to_pk_hex(self):
+        """ Test that decoding a WIF string to a private key hex works as intended """
+        self.assertEquals(gen.wif_to_pk_hex(self.mocked_wif), self.expected_pk_hex)
+
+
+class PublicKeyTests(unittest.TestCase):
+    def setUp(self):
+        self.mocked_pk = [12, 40, 252, 163, 134, 199, 162, 39, 96, 11, 47, 229, 11, 124, 174, 17, 236, 134, 211, 191,
+                          31, 190, 71, 27, 232, 152, 39, 225, 157, 114, 170, 29]
+        self.mocked_hex = '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
+        self.mocked_wif = '5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ'
+        self.expected_public_key = '1GAehh7TsJAHuUAeKZcXf5CnwuGuGgyX2S'
+
+    def test_from_pk(self):
+        self.assertEquals(gen.public_key(pk=self.mocked_pk), self.expected_public_key)
+
+    def test_from_hex(self):
+        self.assertEquals(gen.public_key(_hex=self.mocked_hex), self.expected_public_key)
+
+    def test_from_wif(self):
+        self.assertEquals(gen.public_key(wif=self.mocked_wif), self.expected_public_key)
 
 if __name__ == '__main__':
     unittest.main()
